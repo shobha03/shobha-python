09398569394537c1dd721182a4d2e5715e1c91b0
shobha03 <90245869+shobha03@users.noreply.github.com>
2021-09-08 19:37:36 +0530

    Add files via upload
    3-09-2021 done by shobha

---
+++ day3.py
@@ -0,0 +1,16 @@
+1.implement a logic given year is leap or not?
+
+a=int(input("enter the year"))
+if(a%4==0):
+     print("it is leap year")
+else:
+    print("it is not leap year")
+
+
+    2.print number in revrse order?
+
+    for a in range(10,-1,-1):
+          print(a)
+
+
+3. findout numbers which are divisible by 3 and 9 in given range?
