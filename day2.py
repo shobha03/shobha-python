a7f890c8ce4d8867cf041301d3314c6aad325901
shobha03 <90245869+shobha03@users.noreply.github.com>
2021-09-14 12:00:32 +0530

    Add files via upload
    02-09-2021 done by shobha

---
+++ day2.py
@@ -0,0 +1,303 @@
+
+
+1.You have to printa logic to perform all arthematic operations like add,sub,multi,division,%,//,**
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=a+b
+d=a-b
+e=a*b
+f=a%b
+g=a**b
+h=a//b
+print("adding of two numbers:",c)
+print("substraction of two numbers:",d)
+print("multiplication of two numbers:",e)
+print("division of two numbers:",f)
+print("power of two numbers:",g)
+print("modules of two numbers:",h)
+
+output::
+C:\Users\Sobha\Desktop>bigg.py
+enter the value:12
+enter the value:13
+adding of two numbers: 25
+substraction of two numbers: -1
+multiplication of two numbers: 156
+division of two numbers: 12
+power of two numbers: 106993205379072
+modules of two numbers: 0
+
+
+2.WAP biggest of two numbers?
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if(a<b):
+   print("b is biggest value then a")
+else:
+   print("a is biggest value then b")
+
+output::
+C:\Users\Sobha\Desktop>code.py
+enter the value of a:21
+enter the value of b:12
+a is biggest value then b
+
+
+3.WAP biggest of three numbers?
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+if(a<b):
+   print("b is biggest value then a")
+elif(a<c):
+   print("c is biggest value then a")
+else:
+   print("a is biggest value then c")
+
+output::
+C:\Users\Sobha\Desktop>code.py
+enter the value of a:21
+enter the value of b:12
+enter the value of c:10
+a is biggest value then c
+
+4.WAP generate numbers in given range?
+
+a=5
+for a in range(5,100):
+    print(a)
+outpu::
+
+C:\Users\Sobha\Desktop>range.py
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+
+5.WAP Generate even or odd upto range?
+
+for a in range(1,100):
+     if(a%2==0):
+        print("the even numbers:",a)
+     if(a%2==1):
+        print("the odd numbers:",a)
+output::
+C:\Users\Sobha\Desktop>odd.py
+the odd numbers: 1
+the even numbers: 2
+the odd numbers: 3
+the even numbers: 4
+the odd numbers: 5
+the even numbers: 6
+the odd numbers: 7
+the even numbers: 8
+the odd numbers: 9
+the even numbers: 10
+the odd numbers: 11
+the even numbers: 12
+the odd numbers: 13
+the even numbers: 14
+the odd numbers: 15
+the even numbers: 16
+the odd numbers: 17
+the even numbers: 18
+the odd numbers: 19
+the even numbers: 20
+the odd numbers: 21
+the even numbers: 22
+the odd numbers: 23
+the even numbers: 24
+the odd numbers: 25
+the even numbers: 26
+the odd numbers: 27
+the even numbers: 28
+the odd numbers: 29
+the even numbers: 30
+the odd numbers: 31
+the even numbers: 32
+the odd numbers: 33
+the even numbers: 34
+the odd numbers: 35
+the even numbers: 36
+the odd numbers: 37
+the even numbers: 38
+the odd numbers: 39
+the even numbers: 40
+the odd numbers: 41
+the even numbers: 42
+the odd numbers: 43
+the even numbers: 44
+the odd numbers: 45
+the even numbers: 46
+the odd numbers: 47
+the even numbers: 48
+the odd numbers: 49
+the even numbers: 50
+the odd numbers: 51
+the even numbers: 52
+the odd numbers: 53
+the even numbers: 54
+the odd numbers: 55
+the even numbers: 56
+the odd numbers: 57
+the even numbers: 58
+the odd numbers: 59
+the even numbers: 60
+the odd numbers: 61
+the even numbers: 62
+the odd numbers: 63
+the even numbers: 64
+the odd numbers: 65
+the even numbers: 66
+the odd numbers: 67
+the even numbers: 68
+the odd numbers: 69
+the even numbers: 70
+the odd numbers: 71
+the even numbers: 72
+the odd numbers: 73
+the even numbers: 74
+the odd numbers: 75
+the even numbers: 76
+the odd numbers: 77
+the even numbers: 78
+the odd numbers: 79
+the even numbers: 80
+the odd numbers: 81
+the even numbers: 82
+the odd numbers: 83
+the even numbers: 84
+the odd numbers: 85
+the even numbers: 86
+the odd numbers: 87
+the even numbers: 88
+the odd numbers: 89
+the even numbers: 90
+the odd numbers: 91
+the even numbers: 92
+the odd numbers: 93
+the even numbers: 94
+the odd numbers: 95
+the even numbers: 96
+the odd numbers: 97
+the even numbers: 98
+the odd numbers: 99
+
+6.Generate maath tablefor a given range?
+b=5
+for a in range(1,20):
+    print(b,'x',a,'=',b*a)
+outpu::
+C:\Users\Sobha\Desktop>math.py
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+5 x 11 = 55
+5 x 12 = 60
+5 x 13 = 65
+5 x 14 = 70
+5 x 15 = 75
+5 x 16 = 80
+5 x 17 = 85
+5 x 18 = 90
+5 x 19 = 95
